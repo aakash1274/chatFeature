9130f5fd7f501471415f2c23ec8170d24487e6ef
aakash1274 <157947718+aakash1274@users.noreply.github.com>
2025-01-02 09:57:50 +0000

    sdfdsf

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    firebaseConfig:
+    {
+        "apiKey": "AIzaSyDbhE0zYV3Gn-QdDKWZPiw5hd5avKuxAPI",
+        "authDomain": "chat-b9431.firebaseapp.com",
+        "projectId": "chat-b9431",
+        "storageBucket": "chat-b9431.firebasestorage.app",
+        "messagingSenderId": "716914957082",
+        "appId": "1:716914957082:web:04a6a8e48a043b1a1e2aa3",
+        "measurementId": "G-BLB8VX2HDK"
+    }
+
+};
